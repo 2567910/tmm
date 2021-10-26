caaf09f767b4e194494b2e1d17aadd1cfb93ec50
Lukas Seyfarth <lukas.seyfarth@blu-beyond.com>
2021-10-26 18:08:04 +0200

    added import/ export

--- tmm/apps/core/admin.py
+++ tmm/apps/core/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from tmm.apps.core.models import Translations
 
 from parler.admin import TranslatableAdmin
+from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ExportActionMixin
+from import_export import resources
+from parler.admin import SortedRelatedFieldListFilter
 
 # class ProjectAdmin(TranslatableAdmin):
 #     list_display = (['key', 'value'])
@@ -14,7 +18,23 @@ from parler.admin import TranslatableAdmin
 
 # admin.site.register(Translations, ProjectAdmin)
 
-@admin.register(Translations)
-class TranslationsAdmin(TranslatableAdmin):
-    list_display = (['key', ])
+class TranslationsResource(resources.ModelResource):
+
+    class Meta:
+        model = Translations
+        # fields = ('key', 'value')
+
+class TranslationsAdmin(TranslatableAdmin, ImportExportModelAdmin, ExportActionMixin):
+    resources_class = TranslationsResource
+
+    list_display = ('key', 'value')
+    fieldsets = (
+        (None, {
+            'fields': ('key', 'translations__value'),
+        }),
+    )
+
     search_fields = ('translations__value', 'key')
+
+
+admin.site.register(Translations, TranslationsAdmin)
--- tmm/settings/base.py
+++ tmm/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'tmm.apps.core',
     'parler',
+    'import_export',
 
 ]
 
