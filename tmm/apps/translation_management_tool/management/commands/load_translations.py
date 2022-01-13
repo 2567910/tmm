5b133e8b7ed71c21fbe229909d097e48657ae36b
Lukas Seyfarth <lukas.seyfarth@blu-beyond.com>
2022-01-13 12:10:42 +0100

    cleanup

--- tmm/apps/translation_management_tool/management/commands/load_translations.py
+++ tmm/apps/translation_management_tool/management/commands/load_translations.py
@@ -10,14 +10,8 @@ from tmm.apps.translation_management_tool.models import Project, TranslationKey,
 
 LOGGER = logging.getLogger(__name__)
 
-
-#./manage.py load_translations import_de.json
-
-
-
 class Command(BaseCommand):
     help = 'Load translations from JSON file'
-
 
     def add_arguments(self, parser):
         parser.add_argument('file', type=str)
@@ -26,25 +20,15 @@ class Command(BaseCommand):
         parser.add_argument('--clear', '-c', action='store_true',
                             help='Clear existing objects before loading')
 
-
     def handle(self, *args, **options):
-
         resultDict = {}
-
         filepath = options['file']
         LOGGER.info('Loading tags from %s ...', filepath)
         with transaction.atomic():
             with open(filepath, 'r') as json_in:
                 rawDict = json.load(json_in)
-
-                # filename = os.path.basename(filepath)
-                # project_lang = os.path.splitext(filename)[0]
-                # project_and_lang = project_lang.split('_')  # returns array
                 project_name = options['project']
                 lang_str = options['lang']
-
-
-                LOGGER.info('Loading translation for project %s and language %s...', project_name, lang_str)
 
                 project = Project.objects.filter(name=project_name).first()
                 if not project:
@@ -55,55 +39,36 @@ class Command(BaseCommand):
                     raise Exception('Language does not exist: ', lang_str)
 
                 if options['clear']:
-                    LOGGER.warning('DELETING translations for project %s and language %s', project, lang)
                     deleted = Translation.objects.filter(key__project=project, language=lang).delete()
-                    LOGGER.warning('DELETED %d translations', deleted[0])
 
-
-                for raw_key, raw_value in rawDict.items():  # root/namespace level, e.g. "general"
-                    LOGGER.debug('>>> in loop --- %s : %s', raw_key, raw_value)
-
+                for raw_key, raw_value in rawDict.items():
                     root_key = TranslationKey.objects.filter(key=raw_key, project=project, depth=1).first()
+
                     if not root_key:
                         root_key = TranslationKey.add_root(key=raw_key, project=project)
-                    LOGGER.debug('>>> adding root %s', root_key)
 
                     if isinstance(raw_value, str):
-                        # this is a translation
-                        LOGGER.debug('>>> sinstance if str %s - %s: %s', lang, root_key, raw_value)
                         Translation.objects.filter(key=root_key, language=lang).update(value=raw_value)
 
-                        LOGGER.debug('>>> adding translation %s - %s: %s', lang, root_key, raw_value)
                     elif isinstance(raw_value, dict):
-                        LOGGER.debug('>>> is dict --- %s : %s', raw_value, raw_value)
                         # recurse into children
                         self.create_child(root_key, raw_value, lang)
                     else:
                         raise Exception('Expecting string or dictionary but got', raw_value)
 
-                    # resultDict = get_formated_data_recursive(resultDict, key, rawDict[key])
                     if (type(rawDict[raw_key]) is not dict):
                         resultDict[raw_key] = {"value": rawDict, "parent": None}
-                        print("key: ", raw_key, " value: ", rawDict[raw_key], " No parents")
 
-                    # print(key, rawDict)
-                    # get_formated_data_recursive()
-                    # print(data.get('model'))
-                    # load_data_recursive(data, None)
-                    # Translation.objects.bulk_create(new_objects) # good performance for real production/large files
 
     def create_child(self, parent_node: TranslationKey, raw_dict: dict, lang: Language):
         for child_key, child_value in raw_dict.items():
             child_node = parent_node.get_children().filter(key=child_key, project=parent_node.project).first()
             if not child_node:
                 child_node = parent_node.add_child(key=child_key, project=parent_node.project)
-            LOGGER.debug('>>> adding child %s', child_node.key)
+
             if isinstance(child_value, str):
-                # this is a translation
-                LOGGER.debug('>>> adding child %s', child_node)
-                # Translation.objects.create(key=child_node, value=child_value, language=lang)
                 Translation.objects.filter(key=child_node, language=lang).update(value=child_value)
-                LOGGER.debug('>>> adding translation %s - %s: %s', lang, child_node, child_value)
+
             elif isinstance(child_value, dict):
                 self.create_child(child_node, child_value, lang)
             else:
--- tmm/apps/translation_management_tool/models.py
+++ tmm/apps/translation_management_tool/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 from django.core import management
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.safestring import mark_safe
-import json
 from django.conf.global_settings import LANGUAGES
 from treebeard.mp_tree import MP_Node
 from simple_history.models import HistoricalRecords
-from simple_history import register
 from django.utils import timezone
 
 
@@ -21,7 +18,6 @@ class Language(models.Model):
 
 
 class Project(models.Model):
-
     name = models.CharField(unique=True, db_index=True, max_length=255)
     languages = models.ManyToManyField(Language)
 
@@ -35,39 +31,22 @@ class Project(models.Model):
 
 class TranslationKey(MP_Node): #
     key = models.CharField(max_length=255, help_text=mark_safe(
-        "<b>Do not use the following endings in key: </b><br> $t(KEY_NAME) replace KEY_NAME with a  different translation key to use it in this translation</p>"
-        + "<p><b>Interpolate: </b><br> {{VALUE}} replace VALUE with a variable that is defined for this translation.</p> "
-        + "<p>For more information about the i18next value standart <a href='https://www.i18next.com/misc/json-format' target='_blank'>click here</a>.</p>"))
+        "<p>For more information about the i18next value options <a href='https://www.i18next.com/misc/json-format' target='_blank'>click here</a>.</p>"))
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
-    # node_order_by = ['key']
     class Meta:
         verbose_name = "Translationkey"
         verbose_name_plural = "Translationkeys"
         unique_together = ['project', 'key', 'depth']
 
-    # Translation.objects.create({})
-
     def __str__(self):
         return f"{self.project.name}: {self.key} ({self.depth})"
 
 class Translation(models.Model):
-
     key = models.ForeignKey(TranslationKey, on_delete=models.CASCADE)
     value = models.TextField(max_length=255, blank=True, null=True)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     history = HistoricalRecords()
-
-    # languagesForThisProject = ["1", "2", ""]
-    # # Project.objects.get(name=project)
-
-
-    # for label in languagesForThisProject: # AttributeError: 'ForeignKey' object has no attribute 'languages'
-    #      locals()[label] = models.CharField(max_length=255, null=True, blank=True)
-
-    #      del locals()['label']
-
-    # print(projectLanguages)
 
     class Meta:
         verbose_name = "Translation"
@@ -78,36 +57,15 @@ class Translation(models.Model):
         return f"{self.key} {self.language}"
 
 class JSONImport(models.Model):
-
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     file = models.FileField()
     created_at = models.DateTimeField(default=timezone.now)
-    # languagesForThisProject = ["1", "2", ""]
-    # # Project.objects.get(name=project)
 
     def save(self):
-            # Here implement data handling.
-
-            # run the LoadTranslationsCommand with the uploaded file
-            print(self.file.path)
-            # print("awDict1")
-            # with open(self.file.path, 'r') as json_in:
-            #     print("awDict2")
-            #     rawDict = json.load(json_in)
-            #     print(rawDict)
-
-
-            super().save()
-
-            management.call_command('load_translations', self.file.path, self.language.code,  self.project.name)
-
-    # for label in languagesForThisProject: # AttributeError: 'ForeignKey' object has no attribute 'languages'
-    #      locals()[label] = models.CharField(max_length=255, null=True, blank=True)
-
-    #      del locals()['label']
-
-    # print(projectLanguages)
+        print(self.file.path)
+        super().save()
+        management.call_command('load_translations', self.file.path, self.language.code,  self.project.name)
 
     class Meta:
         verbose_name = "JSON Import"
