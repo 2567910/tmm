1d5844a3dc1b5f2f43bd69e35b859e1f9a2ad00c
2567910 <luki.seyfarth@web.de>
2022-03-08 13:22:22 +0100

    init

--- tmm/apps/translation_management_tool/api/urls.py
+++ tmm/apps/translation_management_tool/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from tmm.apps.translation_management_tool.api.views import TranslationsView
+from tmm.apps.translation_management_tool.api.views import translations_view
 
 urlpatterns = [
-    url(r'^(?P<project>\w+)/(?P<lang>[\w-]+)/$', TranslationsView.as_view()),
+    url(r'^(?P<project>\w+)/(?P<lang>[\w-]+)/$', translations_view),
 ]
--- tmm/apps/translation_management_tool/api/views.py
+++ tmm/apps/translation_management_tool/api/views.py
@@ -1,19 +1,23 @@
 from rest_framework.views import APIView
-from tmm.apps.translation_management_tool.models import Translation
+from tmm.apps.translation_management_tool.models import Translation, Project
 from tmm.apps.translation_management_tool.api.serializer import TranslationsSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, ParseError
 
-def get_keys(obj):
-    arr = []
-    if (obj.key.get_ancestors()):
-        for an in obj.key.get_ancestors():
-            arr.append(an.key)
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+
+def get_keys_from_ancestors(obj):
+    ancestors = obj.key.get_ancestors()
+    if ancestors:
+        arr = [an.key for an in ancestors]
         arr.append(obj.key.key)
         return arr
-    else:
-        arr.append(obj.key.key)
-        return arr
+
+    return [obj.key.key]
 
 
 def set_value_in_dict_recursive(i18nextDict, keysArray, value):
@@ -27,40 +31,63 @@ def set_value_in_dict_recursive(i18nextDict, keysArray, value):
     return i18nextDict
 
 
-class TranslationsView(APIView):
-    # GET /translations/project/lang
-    serializer_class = TranslationsSerializer
+# class TranslationsView(APIView):
+#     # GET /translations/project/lang
+#     serializer_class = TranslationsSerializer
 
-    def get_queryset(self):
-        translations = Translation.objects.all()
-        return translations
+#     def get_queryset(self):
+#         translations = Translation.objects.all()
+#         return translations
 
-    def get(self, request, *args, **kwargs):
+def translations_view(request, *args, **kwargs):
 
-        try:
-            lang = self.kwargs.get("lang")
-            project = self.kwargs.get("project")
+    try:
+        lang = kwargs.get("lang")
+        project_name = kwargs.get("project")
 
-            translations_raw = Translation.objects.all().filter(
-                language__code=lang, key__project__name=project)
-            translation_list = list(translations_raw)
+        project = Project.objects.get_or_404(name=project_name)
 
-            translations_sortet_by_depth = sorted(
-                translation_list, key=lambda d: d.key.get_depth())
+        translations_raw = Translation.objects.filter(
+            language__code=lang, key__project_id=project.id)
+        translation_list = list(translations_raw)
 
-            i18nextDict = dict()
+        translations_sortet_by_depth = sorted(
+            translation_list, key=lambda d: d.key.get_depth())
 
-            for translation in translations_sortet_by_depth:
-                keys = get_keys(translation)
+        i18nextDict = dict()
 
-                i18nextDict = set_value_in_dict_recursive(
-                    i18nextDict, keys, translation.value)
-                print(i18nextDict)
+        # TEST Start --------------------------------------------------
 
-            if(len(i18nextDict) == 0):
-                raise ValidationError
+        # for translation in translation_list:
+        #     path = translation.key.path
+        #     LOGGER.info('path %s', path)
 
-            return Response(i18nextDict)
 
-        except:
+        # only_root_translations = list(filter(lambda translation: translation.key.depth == 1, translation_list))
+
+        # for translation in only_root_translations:
+        #     LOGGER.info(translation.key.key)
+        #     # keys = get_keys_from_ancestors(translation)
+        #     test = translation.key.dump_bulk(parent=translation.key, keep_ids=True)
+        #     LOGGER.info(test)
+        #     keys = get_keys_from_ancestors(tanslation)
+        #     set_value_in_dict_recursive(
+        #         i18nextDict, keys, tanslation.value)
+
+
+        # TEST END --------------------------------------------------
+
+        for translation in translations_sortet_by_depth:
+            keys = get_keys_from_ancestors(translation)
+
+            i18nextDict = set_value_in_dict_recursive(
+                i18nextDict, keys, translation.value)
+            print(i18nextDict)
+
+        if(len(i18nextDict) == 0): # if no translations found
             raise ValidationError
+
+        return Response(i18nextDict)
+
+    except:
+        raise ValidationError
