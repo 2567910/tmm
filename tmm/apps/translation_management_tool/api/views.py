c3661d319f29e1f6b35169f058d56b906346d1d1
2567910 <luki.seyfarth@web.de>
2022-03-09 21:29:06 +0100

    i dont get it...

--- tmm/apps/translation_management_tool/api/views.py
+++ tmm/apps/translation_management_tool/api/views.py
@@ -20,10 +20,13 @@ def set_bulk_in_dict_recursive(i18nextDict, bulk, root_translation_list):
         key = object["data"]["key"]
         root_translation = {}
 
-        for root_translation in root_translation_list:
-            if(root_translation.key.key == key):
-                root_translation = root_translation
-                break
+
+        # for root_translation in root_translation_list:
+        #     if(root_translation.key.key == key):
+        #         root_translation = root_translation
+        #         break
+
+        # root_translation_list.remove(root_translation)
 
         try:
             i18nextDict[key] = dict()
@@ -32,7 +35,7 @@ def set_bulk_in_dict_recursive(i18nextDict, bulk, root_translation_list):
         except:
             i18nextDict[key] = root_translation.value
 
-        root_translation_list.remove(root_translation)
+
 
 
     return i18nextDict
@@ -54,13 +57,14 @@ def translations_view(request, *args, **kwargs):
     #LOG: statement: SELECT 'translation_management_tool_translation"."id", "translation_management_tool_translation"."key_id", "translation_management_tool_translation"."value", "translation_management_tool_translation"."language_id" FROM "translation_management_tool_translation" INNER JOIN "translation_management_tool_translationkey" ON ("translation_management_tool_translation"."key_id" = "translation_management_tool_translationkey"."id") INNER JOIN "translation_management_tool_project" ON ("translation_management_tool_translationkey"."project_id" = "translation_management_tool_project"."id") INNER JOIN "translation_management_tool_language" ON ("translation_management_tool_translation"."language_id" = "translation_management_tool_language"."id") WHERE ("translation_management_tool_translationkey"."depth" = 1 AND "translation_management_tool_project"."name" = 'test' AND "translation_management_tool_language"."code" = 'de')
 
     translations_raw = Translation.objects.filter(
-        language__code=lang, key__project__name=project_name)
+        language__code=lang, key__project__name=project_name).order_by('key__depth')
     translation_list = list(translations_raw)
 
     i18nextDict = dict()
 
     for translation in translation_list:
         LOGGER.info(translation.key.key)
+        LOGGER.info("this should only run for root keys")
         bulk = translation.key.dump_bulk(parent=translation.key, keep_ids=False)
         set_bulk_in_dict_recursive(i18nextDict, bulk, translation_list)
 
